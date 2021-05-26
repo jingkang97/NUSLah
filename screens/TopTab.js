6029629a49c6324fe118d7bd63867309321ebaf8
jingkang97 <jingkang@lomotif.com>
2021-05-26 23:16:12 +0800

    style top bar

--- screens/TopTab.js
+++ screens/TopTab.js
@@ -7,14 +7,27 @@ import Core from './moduleTabs/Core'
 import Electives from './moduleTabs/Electives'
 import Unrestricted from './moduleTabs/Unrestricted'
 import None from './NoPage'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 
 const Tab = createMaterialTopTabNavigator();
 
 const Tabs = () => {
     return ( 
-        <Tab.Navigator >
-            <Tab.Screen name = "Core" component = {Core}/>
+        <Tab.Navigator 
+        activeColor="#39A0ED"
+        tabBarOptions = {{
+            style: {backgroundColor: '#F3F2F2',shadowColor: 'transparent'},
+            activeTintColor: '#39A0ED',
+            inactiveTintColor: '#A5A5A5',
+            indicatorStyle: {width: 50}
+            }}>
+            <Tab.Screen name = "Core" component = {Core} 
+                options={{
+                    backgroundColor: 'black',
+                    tabBarLabel: 'Core',
+                }}
+            />
             <Tab.Screen name = "Electives" component = {Electives}/>
             <Tab.Screen name = "Unrestricted" component = {Unrestricted}/>
         </Tab.Navigator>
@@ -25,8 +38,38 @@ const MainScreenNavigator = createStackNavigator()
 
 const MainScreen = () => {
     return ( 
-        <MainScreenNavigator.Navigator>
-            <MainScreenNavigator.Screen name = "Module Review" component = {Tabs}/>
+        <MainScreenNavigator.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#F3F2F2",
+                    shadowColor: 'transparent'
+                }
+            }}
+        >
+            <MainScreenNavigator.Screen name = "Module Review" component = {Tabs}
+            options={{
+                headerTitle: "NUSLah",
+                headerStatusBarHeight: 60,
+                headerTintColor: 'transparent',
+                headerLeft: () => (
+                  // <Icon.Button name = "ios-menu" size = {30} color = "black"
+                  // backgroundColor = "#F3F2F2" onPress = {()=> navigation.openDrawer()}/>
+                  <View style = {{flexDirection: "row", paddingLeft: 10, height: 50, left: 10}}>
+                    <Text style = {{fontSize : 30, fontWeight: '500'}}>NUS</Text><Text style = {{fontSize : 30, color: '#39A0ED',fontWeight: '500'}}>Lah</Text>
+                  </View>
+                ),
+                headerRight: () => (
+                  <View style = {{flexDirection: 'row'}}>
+                    <Icon.Button name = "ios-notifications-outline" size = {25} 
+                    style = {{marginRight:-12}} color = "black"
+                    backgroundColor = "#F3F2F2" onPress = {console.log('notif!')}></Icon.Button>
+                    <Icon.Button name = "ios-chatbox-ellipses-outline" size = {25} 
+                    color = "black"
+                    backgroundColor = "#F3F2F2" onPress = {console.log('message!')}></Icon.Button>
+                  </View>
+                )
+              }}
+            />
         </MainScreenNavigator.Navigator>
      );
 }
