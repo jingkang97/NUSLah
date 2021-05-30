84da2ed08fd1061b179391a0c24409cd70f4e7ee
jingkang97 <jingkang@lomotif.com>
2021-05-30 10:51:28 +0800

    added flatlist for core

--- screens/moduleTabs/Core.js
+++ screens/moduleTabs/Core.js
@@ -1,5 +1,6 @@
 import React , {useState, useEffect} from 'react';
-import { StyleSheet, Text, SafeAreaView, View, Dimensions} from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, Dimensions, FlatList} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
 import axios from 'axios';
 
 
@@ -19,17 +20,24 @@ const Core = () => {
         })
     }
     useEffect(() => {
-        // getData()
-    })
+        getData()
+    },[])
 
     return ( 
-        <View style = {styles.container}>
-            <Text>
-                Core
-                {reviews.map((review,index)=>(
-                    <p key = {index}>{review.name}</p>
-                ))}
-            </Text>
+        <View style = {styles.container}
+        >
+            {/* flatlist need to use item */}
+            <FlatList data = {reviews}
+            renderItem={({item})=>(
+                <View style = {styles.listItem}>
+                    <Text> {item.name}</Text>
+                    <Icon.Button name = "chevron-forward-outline" color = 'black'  backgroundColor= 'transparent'/>
+                </View>
+                
+            )}
+            >
+
+            </FlatList>
         </View>
      );
 }
@@ -37,12 +45,23 @@ const Core = () => {
 
 
 const styles = StyleSheet.create({
-    container: {
+    container:{
+        flex: 1, 
+        backgroundColor: '#F3F2F2',
+        paddingTop: 20
+        // margin: 10
+
+    },
+    listItem:{
+        margin: 10,
+        padding: 10,
+        backgroundColor: "#FFF",
+        width: "90%",
         flex: 1,
-        // backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
+        alignSelf: "center",
+        flexDirection: "row",
+        borderRadius: 5
+    }
 })
  
 export default Core;
